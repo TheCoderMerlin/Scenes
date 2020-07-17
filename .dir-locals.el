6ea2a14260c2736c0d67c60d4c77086c4b1dd48f
David Ben-Yaakov <david@tangogolfdigital.com>
2020-07-17 14:31:15 -0500

    Removed dir-locals as it can be generated

--- .dir-locals.el
+++
@@ -1,8 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((swift-mode . 
-  ((lsp-sourcekit-extra-args . (
-	 "-Xlinker" "-L" "-Xlinker" "/usr/local/lib/merlin/Igis-1.3.4/Igis/.build/debug" "-Xswiftc" "-I" "-Xswiftc" "/usr/local/lib/merlin/Igis-1.3.4/Igis/.build/debug" 
-
-)))))
