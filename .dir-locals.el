8506cf8c1824500fc3b50623bf35ca3bebe5e6e7
David Ben-Yaakov <david@tangogolfdigital.com>
2020-07-17 13:20:38 -0500

    Update .dir-locals for emacs lsp

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,8 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((swift-mode
-  (flycheck-swift-include-search-paths  "/usr/local/lib/merlin/Igis-1.3.4/Igis/.build/debug")))
+((swift-mode . 
+  ((lsp-sourcekit-extra-args . (
+	 "-Xlinker" "-L" "-Xlinker" "/usr/local/lib/merlin/Igis-1.3.4/Igis/.build/debug" "-Xswiftc" "-I" "-Xswiftc" "/usr/local/lib/merlin/Igis-1.3.4/Igis/.build/debug" 
+
+)))))
